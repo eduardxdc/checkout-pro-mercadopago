39a22f9ea8adaf1c74b03201bceb56c1e9ca0f95
eduardxdc (afa) <eduardxdc@proton.me>
2024-04-30 18:04:07 -0300

    feat: integrator_id in json

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -20,10 +20,11 @@ export const createOrder = (req, res) => {
             },
           ],
           excluded_payment_types: [],
-          installments: 5,
+          installments: 6,
         },
         items: [
           {
+            id: "1907",
             title: "Product",
             quantity: 1,
             unit_price: 1000,
@@ -36,7 +37,7 @@ export const createOrder = (req, res) => {
           pending: `${HOST}/pending`,
         },
         auto_return: "approved",
-        notification_url: "https://f63d-170-247-194-174.ngrok-free.app/webhook",
+        notification_url: "https://154d-2001-1284-f50e-31bf-509f-c9b2-c9cf-473a.ngrok-free.app/webhook",
         external_reference: "eduardxdc@gmail.com",
       },
     })
@@ -56,11 +57,13 @@ export const receiveWebhook = async (req, res) => {
   try {
     if (payment.type === "payment") {
       const id = payment["data.id"];
-      const accessToken = "APP_USR-8693778713813598-042714-56aa663e3f7c0fc3778dc68b561df61c-1788151692"; // Substitua pelo seu token de acesso
+      const accessToken = MERCADOPAGO_API_KEY;
+      const integrator_id = INTEGRATOR_ID;
 
       const response = await axios.get(`https://api.mercadopago.com/v1/payments/${id}`, {
         headers: {
-          'Authorization': `Bearer ${accessToken}`
+          'Authorization': `Bearer ${accessToken}`,
+          'x-integrator-id': integrator_id,
         }
       });
 
